3cfb5ca3f5224dc319e7a1f72d2e0540db179f90
Sameer9995 <73990147+Sameer9995@users.noreply.github.com>
2020-12-10 20:20:53 +0530

    Add files via upload

---
+++ passrfail.java
@@ -0,0 +1,18 @@
+import java.util.scanner;
+public class result {
+    
+ public static void main(String)[] args) {
+  int num;
+  scanner reader = new Scanner(System.in);
+  System.out.printIn("Enter score: ");
+  num = reader.nextInt();
+  
+  if (num>=40)
+  {
+    system.out.printIn("Pass!");  
+  }
+  else
+   system.out.printIn("Fail!");
+ }
+ 
+}
