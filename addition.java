60460e4e89dd1d95785f43d3c6b2e377dbd6663b
Sameer9995 <73990147+Sameer9995@users.noreply.github.com>
2020-12-10 19:31:22 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,9 @@
+ public class addition
+{
+   public static void main(String args[]) 
+   { 
+      int x=25,y=15,add;
+      add=x+y;
+      System.out.println("Sum of these numbers: "+add);
+   }
+}   
